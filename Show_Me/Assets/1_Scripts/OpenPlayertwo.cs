89f1e36f31b246d52b786b60cfefde3348dbc8bf
Mees Hollebeek <Mees.hollebeek@gmail.com>
2022-11-02 14:37:25 +0100

    Done detecting amount of players pulling

---
+++ Show_Me/Assets/1_Scripts/OpenPlayertwo.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPlayertwo : MonoBehaviour
+{
+    //opening gate Player 2/1
+
+    public static bool player = true;
+    public static bool Exit = true;
+
+
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("OpeningGateP1");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && OpenPlayerOne.occupied)
+        {
+            Debug.Log("EnterP1");
+            player = false;
+            Exit = true;
+
+
+        }
+
+        if (other.CompareTag("Player") && !OpenPlayerOne.occupied)
+        {
+            Debug.Log("EnterP2");
+            Exit = false;
+            player = false;
+
+            //door open
+
+        }
+
+
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("exitP1");
+          
+                player = true;
+                Exit = true;
+            
+           
+            
+            //door close
+        }
+
+    }
+}
