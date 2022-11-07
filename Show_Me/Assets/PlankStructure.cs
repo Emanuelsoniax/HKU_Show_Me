72753067c02249e6d43dae95762025a299d8845b
Emmie den Hartog <emmie.denhartog@student.hku.nl>
2022-11-07 12:04:53 +0100

    First try at collapsing wall with planks

---
+++ Show_Me/Assets/PlankStructure.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankStructure : MonoBehaviour
+{
+    public float health;
+    public Plank[] planks;
+    List<Rigidbody> planksRB = new List<Rigidbody>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Plank plank in planks)
+        {
+            planksRB.Add(plank.GetComponent<Rigidbody>());
+            plank.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Collapse();
+        }
+    }
+
+    private void Collapse()
+    {
+        foreach(Plank plank in planks)
+        {
+            plank.enabled = true;
+        }
+
+        foreach(Rigidbody rb in planksRB)
+        {
+            rb.useGravity = true;
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Boulder>())
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        health--;
+    }
+}
